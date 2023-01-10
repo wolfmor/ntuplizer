0b492dae1222612f6e39299bd3c499b4866f54d0
altews <alexandra.tews@desy.de>
2023-01-10 13:42:07 +0100

    2023_01_10

--- crabConfig_forHiggsino.py
+++ crabConfig_forHiggsino.py
@@ -4,11 +4,27 @@ import glob
 now = datetime.datetime.now()
 
 config = config()
+#tag = 'test'
 
 ## era 16_UL
+#tag = 'test2'
+
 #tag = 'ZJetsToNuNu_Zpt-200toInf_RunIISummer20UL16RECOAPV_preVFP'
 #tag = 'DYJetsToLL_M-50_Zpt-200toInf_RunIISummer20UL16RECOAPV_preVFP'
 tag = 'TTJets_DiLept_RunIISummer20UL16RECOAPV_preVFP'
+
+#tag = 'WJetsToLNu_HT-200To400_RunIISummer20UL16RECOAPV_preVFP'
+#tag = 'WJetsToLNu_HT-70To100_RunIISummer20UL16RECOAPV_preVFP'
+#tag = 'WJetsToLNu_HT-100to200_RunIISummer20UL16RECOAPV_preVFP'
+#tag = 'WJetsToLNu_HT-600to800_RunIISummer20UL16RECOAPV_preVFP'
+#tag = 'WJetsToLNu_HT-400to600_RunIISummer20UL16RECOAPV_preVFP'
+#tag = 'WJetsToLNu_HT-800to1200_RunIISummer20UL16RECOAPV_preVFP'
+#tag = 'WJetsToLNu_HT-1200to2500_RunIISummer20UL16RECOAPV_preVFP'
+#tag = 'WJetsToLNu_HT-2500toInf_RunIISummer20UL16RECOAPV_preVFP'
+
+#tag = 'SingleMuon_Run2016B_ver1_UL2016_HIPM'
+#tag = 'SingleMuon_Run2016B_ver2_UL2016_HIPM'
+#
 
 ## era 16_07Aug17
 #tag = 'ZJetsToNuNu_Zpt-200toInf_TuneCUETP8M1_13TeV-madgraphMLM-pythia8'
@@ -24,8 +40,10 @@ config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = 'construct_secondary_vertices_cfg.py'
 
 ## era 16_UL
-config.JobType.scriptArgs = ['tag=crab,era16_UL_APV,']
+config.JobType.scriptArgs = ['tag=crab,era16_UL_APV']
+#config.JobType.scriptArgs = ['tag=crab,era16_UL_APV,ZJetsToNuNu_Zpt-200toInf_UL16_preVFP']
 #config.JobType.scriptArgs = ['tag=crab,era16_UL_APV,cleanleptons']
+#config.JobType.scriptArgs = ['tag=crab,era16_UL,data,cleanleptons']
 
 
 ## era 16_07Aug17
@@ -464,16 +482,27 @@ config.JobType.inputFiles = ['/nfs/dust/cms/user/tewsalex/CMSSW_10_2_18/src/ntup
 '/nfs/dust/cms/user/wolfmor/NTupleStuff/PUweights.root'
 ]
 config.JobType.allowUndistributedCMSSW = True
-config.JobType.maxMemoryMB = 3500
+#config.JobType.maxMemoryMB = 3500
 config.JobType.scriptExe = 'allinonejob.sh'
 
 #config.JobType.outputFiles = ['simpleoutput.txt',', 'crab_NTuple.root']
 config.JobType.outputFiles = ['crab_NTuple.root']
 
 ## era 16_UL
+#config.Data.inputDataset = '/WJetsToLNu_HT-2500ToInf_TuneCP5_13TeV-madgraphMLM-pythia8/RunIISummer20UL16RECOAPV-106X_mcRun2_asymptotic_preVFP_v8_ext1-v1/AODSIM'
+#config.Data.inputDataset = '/WJetsToLNu_HT-1200To2500_TuneCP5_13TeV-madgraphMLM-pythia8/RunIISummer20UL16RECOAPV-106X_mcRun2_asymptotic_preVFP_v8_ext1-v1/AODSIM'
+#config.Data.inputDataset = '/WJetsToLNu_HT-800To1200_TuneCP5_13TeV-madgraphMLM-pythia8/RunIISummer20UL16RECOAPV-106X_mcRun2_asymptotic_preVFP_v8_ext1-v1/AODSIM'
+#config.Data.inputDataset = '/WJetsToLNu_HT-400To600_TuneCP5_13TeV-madgraphMLM-pythia8/RunIISummer20UL16RECOAPV-106X_mcRun2_asymptotic_preVFP_v8-v1/AODSIM'
+#config.Data.inputDataset = '/WJetsToLNu_HT-600To800_TuneCP5_13TeV-madgraphMLM-pythia8/RunIISummer20UL16RECOAPV-106X_mcRun2_asymptotic_preVFP_v8_ext1-v1/AODSIM'
+#config.Data.inputDataset = '/WJetsToLNu_HT-100To200_TuneCP5_13TeV-madgraphMLM-pythia8/RunIISummer20UL16RECOAPV-106X_mcRun2_asymptotic_preVFP_v8-v1/AODSIM'
+#config.Data.inputDataset = '/WJetsToLNu_HT-70To100_TuneCP5_13TeV-madgraphMLM-pythia8/RunIISummer20UL16RECOAPV-106X_mcRun2_asymptotic_preVFP_v8-v1/AODSIM'
+#config.Data.inputDataset = '/WJetsToLNu_HT-200To400_TuneCP5_13TeV-madgraphMLM-pythia8/RunIISummer20UL16RECOAPV-106X_mcRun2_asymptotic_preVFP_v8-v1/AODSIM'
+
 config.Data.inputDataset = '/TTJets_DiLept_TuneCP5_13TeV-madgraphMLM-pythia8/RunIISummer20UL16RECOAPV-106X_mcRun2_asymptotic_preVFP_v8-v2/AODSIM'
 #config.Data.inputDataset = '/DYJetsToLL_M-50_Zpt-200toInf_BPSFilter_TuneCP5_13TeV-madgraphMLM-pythia8/RunIISummer20UL16RECOAPV-106X_mcRun2_asymptotic_preVFP_v8-v1/AODSIM'
 #config.Data.inputDataset = '/ZJetsToNuNu_Zpt-200toInf_BPSFilter_TuneCP5_13TeV-madgraphMLM-pythia8/RunIISummer20UL16RECOAPV-106X_mcRun2_asymptotic_preVFP_v8-v1/AODSIM'
+#config.Data.inputDataset = '/SingleMuon/Run2016B-21Feb2020_ver1_UL2016_HIPM-v1/AOD'
+#config.Data.inputDataset = '/SingleMuon/Run2016B-21Feb2020_ver2_UL2016_HIPM-v1/AOD'
 
 ## era 16_07Aug17
 #config.Data.inputDataset = '/SingleMuon/Run2016B-07Aug17_ver2-v1/AOD'
@@ -482,11 +511,12 @@ config.Data.inputDataset = '/TTJets_DiLept_TuneCP5_13TeV-madgraphMLM-pythia8/Run
 
 config.Data.inputDBS = 'global'
 
-config.Data.splitting = 'EventAwareLumiBased'
-config.Data.unitsPerJob = 11
+config.Data.splitting = 'Automatic'
+#config.Data.splitting = 'EventAwareLumiBased'
+#config.Data.unitsPerJob = 10
 #NJOBS = 1
-NJOBS = 1
-config.Data.totalUnits = config.Data.unitsPerJob * NJOBS
+#NJOBS = 1000
+#config.Data.totalUnits = config.Data.unitsPerJob * NJOBS
 
 #config.Data.outLFNDirBase = '/store/user/altews/' 
 config.Data.publication = False # this must be false for additional non-root outputfiles
