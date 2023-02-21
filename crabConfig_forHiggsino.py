b0607fc8af7d2c43fa5b8ee3dffe71b66374dd83
altews <alexandra.tews@desy.de>
2023-02-21 15:59:09 +0100

    cleans up crabconfig, provides crabSubmission for several atasets in one go

--- crabConfig_forHiggsino.py
+++ crabConfig_forHiggsino.py
@@ -4,51 +4,11 @@ import glob
 now = datetime.datetime.now()
 
 config = config()
-#tag = 'test'
 
 ## era 16_UL
-#tag = 'DYJetsToLL_M-50_Zpt-200toInf_RunIISummer20UL16RECOAPV_preVFP'
-#tag = 'TTJets_DiLept_RunIISummer20UL16RECOAPV_preVFP'
-tag = 'ZJetsToNuNu_Zpt-200toInf_BPSFilter_RunIISummer20UL16RECOAPV_preVFP'
+dataset_in = '/MET/Run2016B-21Feb2020_ver1_UL2016_HIPM-v1/AOD'
 
-#tag = 'WJetsToLNu_HT-200To400_RunIISummer20UL16RECOAPV_preVFP'
-#tag = 'WJetsToLNu_HT-70To100_RunIISummer20UL16RECOAPV_preVFP'
-#tag = 'WJetsToLNu_HT-100to200_RunIISummer20UL16RECOAPV_preVFP'
-#tag = 'WJetsToLNu_HT-400to600_RunIISummer20UL16RECOAPV_preVFP'
-#tag = 'WJetsToLNu_HT-600to800_RunIISummer20UL16RECOAPV_preVFP'
-#tag = 'WJetsToLNu_HT-800to1200_RunIISummer20UL16RECOAPV_preVFP'
-#tag = 'WJetsToLNu_HT-1200to2500_RunIISummer20UL16RECOAPV_preVFP'
-#tag = 'WJetsToLNu_HT-2500toInf_RunIISummer20UL16RECOAPV_preVFP'
-
-#tag = 'ZJetsTNuNu_HT-200To400_RunIISummer20UL16RECOAPV_preVFP'
-#tag = 'ZJetsTNuNu_HT-800To1200_RunIISummer20UL16RECOAPV_preVFP'
-#tag = 'ZJetsTNuNu_HT-1200To2500_RunIISummer20UL16RECOAPV_preVFP'
-#tag = 'ZJetsTNuNu_HT-2500ToInf_RunIISummer20UL16RECOAPV_preVFP'
-
-#tag = 'SingleMuon_Run2016B_ver1_UL2016_HIPM'
-#tag = 'SingleMuon_Run2016B_ver2_UL2016_HIPM'
-#tag = 'Test_MET_Run2016E-21Feb2020_UL2016_HIPM'
-
-
-#tag = 'MET_Run2016B-21Feb2020_ver1_UL2016_HIPM'
-#tag = 'MET_Run2016B-21Feb2020_ver2_UL2016_HIPM'
-#tag = 'MET_Run2016C-21Feb2020_UL2016_HIPM'
-#tag = 'MET_Run2016D-21Feb2020_UL2016_HIPM'
-#tag = 'MET_Run2016E-21Feb2020_UL2016_HIPM'
-#tag = 'MET_Run2016F-21Feb2020_UL2016_HIPM'
-#tag = 'MET_Run2016F-21Feb2020_UL2016'
-#tag = 'MET_Run2016G-21Feb2020_UL2016'
-#tag = 'MET_Run2016H-21Feb2020_UL2016'
-
-#tag = 'ZJetsToNuNu_Zpt-200toInf_BPSFilter_RunIISummer20UL16RECO'
-#
-
-## era 16_07Aug17
-#tag = 'ZJetsToNuNu_Zpt-200toInf_TuneCUETP8M1_13TeV-madgraphMLM-pythia8'
-#tag = 'DYJetsToLL_M-50_Zpt-150toInf_TuneCUETP8M1_13TeV-madgraphMLM-pythia8'
-#tag = 'SingleMuon_Run2016B-07Aug17_ver2-v1'
-
-config.General.requestName = tag+'_'+now.strftime("%Y_%m_%d")
+config.General.requestName = 'test_data_'+now.strftime("%Y_%m_%d")
 config.General.workArea = 'crab_projects'
 config.General.transferOutputs = True
 config.General.transferLogs = True
@@ -57,20 +17,15 @@ config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = 'construct_secondary_vertices_cfg.py'
 
 ## era 16_UL
-#config.JobType.scriptArgs = ['tag=crab,era16_UL_APV']
-config.JobType.scriptArgs = ['tag=crab,genmatchalltracks,era16_UL_APV','dataset=/ZJetsToNuNu_Zpt-200toInf_BPSFilter_TuneCP5_13TeV-madgraphMLM-pythia8/RunIISummer20UL16RECOAPV-106X_mcRun2_asymptotic_preVFP_v8-v1/AODSIM']
-#config.JobType.scriptArgs = ['tag=crab,era16_UL_APV,TTJets_DiLept']
-#config.JobType.scriptArgs = ['tag=crab,genmatchalltracks,era16_UL_APV,ZJetsToNuNu_Zpt-200toInf_UL16_preVFP']
-#config.JobType.scriptArgs = ['tag=crab,genmatchalltracks,era16_UL,ZJetsToNuNu_Zpt-200toInf_UL16']
-#config.JobType.scriptArgs = ['tag=crab,era16_UL_APV,cleanleptons,DYJetsToLL_M-50_Zpt-200toInf_UL16_preVFP']
-#config.JobType.scriptArgs = ['tag=crab,era16_UL,data']
-
+#config.JobType.scriptArgs = ['tag=crab,genmatchalltracks,era16_UL_APV','dataset='+dataset_in]
+#config.JobType.scriptArgs = ['tag=crab,genmatchalltracks,era16_UL_APV,cleanleptons', 'dataset='+dataset_in]
+config.JobType.scriptArgs = ['tag=crab,era16_UL,data']
 
 ## era 16_07Aug17
 #config.JobType.scriptArgs = ['tag=crab,era16_07Aug17,data,cleanleptons']
 
  
-config.JobType.inputFiles = ['/nfs/dust/cms/user/tewsalex/CMSSW_10_2_18/src/ntuplizer/plantTrees.py', # if use absolute paths here, only the filename is needed in NTupleizer; not the full path again, max. 120 MB to sandbox
+config.JobType.inputFiles = ['/nfs/dust/cms/user/tewsalex/CMSSW_10_2_18/src/ntuplizer/plantTrees.py', 
 '/nfs/dust/cms/user/tewsalex/CMSSW_10_2_18/src/ntuplizer/commons.py',
 '/nfs/dust/cms/user/wolfmor/NTupleStuff/']
 
@@ -78,62 +33,20 @@ config.JobType.allowUndistributedCMSSW = True
 #config.JobType.maxMemoryMB = 3500
 config.JobType.scriptExe = 'allinonejob.sh'
 
-#config.JobType.outputFiles = ['simpleoutput.txt',', 'crab_NTuple.root']
 config.JobType.outputFiles = ['crab_NTuple.root']
 
 ## era 16_UL
-
-#config.Data.inputDataset = '/WJetsToLNu_HT-2500ToInf_TuneCP5_13TeV-madgraphMLM-pythia8/RunIISummer20UL16RECOAPV-106X_mcRun2_asymptotic_preVFP_v8_ext1-v1/AODSIM'
-#config.Data.inputDataset = '/WJetsToLNu_HT-2500ToInf_TuneCP5_13TeV-madgraphMLM-pythia8/RunIISummer20UL16RECOAPV-106X_mcRun2_asymptotic_preVFP_v8-v1/AODSIM'
-#config.Data.inputDataset = '/WJetsToLNu_HT-1200To2500_TuneCP5_13TeV-madgraphMLM-pythia8/RunIISummer20UL16RECOAPV-106X_mcRun2_asymptotic_preVFP_v8-v1/AODSIM'
-#config.Data.inputDataset = '/WJetsToLNu_HT-800To1200_TuneCP5_13TeV-madgraphMLM-pythia8/RunIISummer20UL16RECOAPV-106X_mcRun2_asymptotic_preVFP_v8-v1/AODSIM'
-#config.Data.inputDataset = '/WJetsToLNu_HT-400To600_TuneCP5_13TeV-madgraphMLM-pythia8/RunIISummer20UL16RECOAPV-106X_mcRun2_asymptotic_preVFP_v8-v1/AODSIM'
-#config.Data.inputDataset = '/WJetsToLNu_HT-600To800_TuneCP5_13TeV-madgraphMLM-pythia8/RunIISummer20UL16RECOAPV-106X_mcRun2_asymptotic_preVFP_v8-v1/AODSIM'
-#config.Data.inputDataset = '/WJetsToLNu_HT-100To200_TuneCP5_13TeV-madgraphMLM-pythia8/RunIISummer20UL16RECOAPV-106X_mcRun2_asymptotic_preVFP_v8-v1/AODSIM'
-#config.Data.inputDataset = '/WJetsToLNu_HT-70To100_TuneCP5_13TeV-madgraphMLM-pythia8/RunIISummer20UL16RECOAPV-106X_mcRun2_asymptotic_preVFP_v8-v1/AODSIM'
-#config.Data.inputDataset = '/WJetsToLNu_HT-200To400_TuneCP5_13TeV-madgraphMLM-pythia8/RunIISummer20UL16RECOAPV-106X_mcRun2_asymptotic_preVFP_v8-v1/AODSIM'
-
-#config.Data.inputDataset = '/TTJets_DiLept_TuneCP5_13TeV-madgraphMLM-pythia8/RunIISummer20UL16RECOAPV-106X_mcRun2_asymptotic_preVFP_v8-v2/AODSIM'
-#config.Data.inputDataset = '/DYJetsToLL_M-50_Zpt-200toInf_BPSFilter_TuneCP5_13TeV-madgraphMLM-pythia8/RunIISummer20UL16RECOAPV-106X_mcRun2_asymptotic_preVFP_v8-v1/AODSIM'
-
-config.Data.inputDataset = '/ZJetsToNuNu_Zpt-200toInf_BPSFilter_TuneCP5_13TeV-madgraphMLM-pythia8/RunIISummer20UL16RECOAPV-106X_mcRun2_asymptotic_preVFP_v8-v1/AODSIM'
-
-#config.Data.inputDataset = '/ZJetsToNuNu_HT-200To400_TuneCP5_13TeV-madgraphMLM-pythia8/RunIISummer20UL16RECOAPV-106X_mcRun2_asymptotic_preVFP_v8-v1/AODSIM'
-#config.Data.inputDataset = '/ZJetsToNuNu_HT-800To1200_TuneCP5_13TeV-madgraphMLM-pythia8/RunIISummer20UL16RECOAPV-106X_mcRun2_asymptotic_preVFP_v8-v3/AODSIM'
-#config.Data.inputDataset = '/ZJetsToNuNu_HT-1200To2500_TuneCP5_13TeV-madgraphMLM-pythia8/RunIISummer20UL16RECOAPV-106X_mcRun2_asymptotic_preVFP_v8-v1/AODSIM'
-#config.Data.inputDataset = '/ZJetsToNuNu_HT-2500ToInf_TuneCP5_13TeV-madgraphMLM-pythia8/RunIISummer20UL16RECOAPV-106X_mcRun2_asymptotic_preVFP_v8-v3/AODSIM'
-
-#config.Data.inputDataset = '/SingleMuon/Run2016B-21Feb2020_ver1_UL2016_HIPM-v1/AOD'
-#config.Data.inputDataset = '/SingleMuon/Run2016B-21Feb2020_ver2_UL2016_HIPM-v1/AOD'
-
-#config.Data.inputDataset = '/MET/Run2016B-21Feb2020_ver1_UL2016_HIPM-v1/AOD'
-#config.Data.inputDataset = '/MET/Run2016B-21Feb2020_ver2_UL2016_HIPM-v1/AOD'
-#config.Data.inputDataset = '/MET/Run2016C-21Feb2020_UL2016_HIPM-v1/AOD'
-#config.Data.inputDataset = '/MET/Run2016D-21Feb2020_UL2016_HIPM-v1/AOD'
-#config.Data.inputDataset = '/MET/Run2016E-21Feb2020_UL2016_HIPM-v1/AOD'
-#config.Data.inputDataset = '/MET/Run2016F-21Feb2020_UL2016_HIPM-v1/AOD'
-#config.Data.inputDataset = '/MET/Run2016F-21Feb2020_UL2016-v1/AOD'
-#config.Data.inputDataset = '/MET/Run2016G-21Feb2020_UL2016-v1/AOD'
-#config.Data.inputDataset = '/MET/Run2016H-21Feb2020_UL2016-v2/AOD'
-
-#config.Data.inputDataset = '/ZJetsToNuNu_Zpt-200toInf_BPSFilter_TuneCP5_13TeV-madgraphMLM-pythia8/RunIISummer20UL16RECO-106X_mcRun2_asymptotic_v13-v1/AODSIM'
-
-## era 16_07Aug17
-#config.Data.inputDataset = '/SingleMuon/Run2016B-07Aug17_ver2-v1/AOD'
-#config.Data.inputDataset = '/DYJetsToLL_M-50_Zpt-150toInf_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISummer16DR80Premix-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/AODSIM'
-#config.Data.inputDataset = '/ZJetsToNuNu_Zpt-200toInf_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISummer16DR80Premix-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/AODSIM'
-
+config.Data.inputDataset = dataset_in
 config.Data.inputDBS = 'global'
 
 config.Data.splitting = 'FileBased'
 config.Data.unitsPerJob = 1
-#NJOBS = 1
-#NJOBS = 1000
-#config.Data.totalUnits = config.Data.unitsPerJob * NJOBS
+NJOBS = 1
+config.Data.totalUnits = config.Data.unitsPerJob * NJOBS
 
-#config.Data.outLFNDirBase = '/store/user/altews/' 
-config.Data.publication = False # this must be false for additional non-root outputfiles
-config.Data.outputDatasetTag = tag + '_'+now.strftime("%Y_%m_%d")
+config.Data.outLFNDirBase = '/store/user/altews/NTuples/NTuplesVXXX/16UL_preAPV/' 
+config.Data.publication = False
+config.Data.outputDatasetTag = now.strftime("%Y_%m_%d")
 
 #config.Site.blacklist = ['T1_RU_JINR']
-config.Site.storageSite = "T2_DE_DESY" # uncomment or put 
+config.Site.storageSite = "T2_DE_DESY" 
--- crabSubmission.py
+++ crabSubmission.py
@@ -13,27 +13,50 @@ tag = ""
 test = False
 
 datasets = [
-			"/ZJetsToNuNu_HT-800To1200_TuneCP5_13TeV-madgraphMLM-pythia8/RunIISummer20UL16RECOAPV-106X_mcRun2_asymptotic_preVFP_v8-v3/AODSIM"
-			,"/ZJetsToNuNu_HT-1200To2500_TuneCP5_13TeV-madgraphMLM-pythia8/RunIISummer20UL16RECOAPV-106X_mcRun2_asymptotic_preVFP_v8-v1/AODSIM"
-			# ,"/ZJetsToNuNu_HT-2500ToInf_TuneCP5_13TeV-madgraphMLM-pythia8/RunIISummer20UL16RECOAPV-106X_mcRun2_asymptotic_preVFP_v8-v3/AODSIM"
-			# ,"/SingleMuon/Run2016B-21Feb2020_ver1_UL2016_HIPM-v1/AOD"
-			# ,"/SingleMuon/Run2016B-21Feb2020_ver2_UL2016_HIPM-v1/AOD"
-			# ,"/MET/Run2016B-21Feb2020_ver1_UL2016_HIPM-v1/AOD"
-			# ,"/MET/Run2016B-21Feb2020_ver2_UL2016_HIPM-v1/AOD"
+			'/ZJetsToNuNu_Zpt-200toInf_BPSFilter_TuneCP5_13TeV-madgraphMLM-pythia8/RunIISummer20UL16RECOAPV-106X_mcRun2_asymptotic_preVFP_v8-v1/AODSIM'
+			,'/ZJetsToNuNu_HT-100To200_TuneCP5_13TeV-madgraphMLM-pythia8/RunIISummer20UL16RECOAPV-106X_mcRun2_asymptotic_preVFP_v8-v1/AODSIM'
+			,'/ZJetsToNuNu_HT-200To400_TuneCP5_13TeV-madgraphMLM-pythia8/RunIISummer20UL16RECOAPV-106X_mcRun2_asymptotic_preVFP_v8-v1/AODSIM'
+			,'/ZJetsToNuNu_HT-400To600_TuneCP5_13TeV-madgraphMLM-pythia8/RunIISummer20UL16RECOAPV-106X_mcRun2_asymptotic_preVFP_v8-v3/AODSIM'
+			,'/ZJetsToNuNu_HT-600To800_TuneCP5_13TeV-madgraphMLM-pythia8/RunIISummer20UL16RECOAPV-106X_mcRun2_asymptotic_preVFP_v8-v3/AODSIM'
+			,'/ZJetsToNuNu_HT-800To1200_TuneCP5_13TeV-madgraphMLM-pythia8/RunIISummer20UL16RECOAPV-106X_mcRun2_asymptotic_preVFP_v8-v3/AODSIM'
+			,'/ZJetsToNuNu_HT-1200To2500_TuneCP5_13TeV-madgraphMLM-pythia8/RunIISummer20UL16RECOAPV-106X_mcRun2_asymptotic_preVFP_v8-v1/AODSIM'
+			,'/ZJetsToNuNu_HT-2500ToInf_TuneCP5_13TeV-madgraphMLM-pythia8/RunIISummer20UL16RECOAPV-106X_mcRun2_asymptotic_preVFP_v8-v3/AODSIM'
+			
+			,'/WJetsToLNu_HT-70To100_TuneCP5_13TeV-madgraphMLM-pythia8/RunIISummer20UL16RECOAPV-106X_mcRun2_asymptotic_preVFP_v8-v1/AODSIM'
+			,'/WJetsToLNu_HT-100To200_TuneCP5_13TeV-madgraphMLM-pythia8/RunIISummer20UL16RECOAPV-106X_mcRun2_asymptotic_preVFP_v8-v1/AODSIM'
+			,'/WJetsToLNu_HT-200To400_TuneCP5_13TeV-madgraphMLM-pythia8/RunIISummer20UL16RECOAPV-106X_mcRun2_asymptotic_preVFP_v8-v1/AODSIM'
+			,'/WJetsToLNu_HT-400To600_TuneCP5_13TeV-madgraphMLM-pythia8/RunIISummer20UL16RECOAPV-106X_mcRun2_asymptotic_preVFP_v8-v1/AODSIM'
+			,'/WJetsToLNu_HT-600To800_TuneCP5_13TeV-madgraphMLM-pythia8/RunIISummer20UL16RECOAPV-106X_mcRun2_asymptotic_preVFP_v8-v1/AODSIM'
+			,'/WJetsToLNu_HT-800To1200_TuneCP5_13TeV-madgraphMLM-pythia8/RunIISummer20UL16RECOAPV-106X_mcRun2_asymptotic_preVFP_v8-v1/AODSIM'
+			,'/WJetsToLNu_HT-1200To2500_TuneCP5_13TeV-madgraphMLM-pythia8/RunIISummer20UL16RECOAPV-106X_mcRun2_asymptotic_preVFP_v8-v1/AODSIM'
+			#,'/WJetsToLNu_HT-2500ToInf_TuneCP5_13TeV-madgraphMLM-pythia8/RunIISummer20UL16RECOAPV-106X_mcRun2_asymptotic_preVFP_v8-v1/AODSIM'
+			,'/WJetsToLNu_HT-2500ToInf_TuneCP5_13TeV-madgraphMLM-pythia8/RunIISummer20UL16RECOAPV-106X_mcRun2_asymptotic_preVFP_v8_ext1-v1/AODSIM' ## To Do submit
+			,'/TTJets_DiLept_TuneCP5_13TeV-madgraphMLM-pythia8/RunIISummer20UL16RECOAPV-106X_mcRun2_asymptotic_preVFP_v8-v2/AODSIM'
+			,'/TTJets_SingleLeptFromT_TuneCP5_13TeV-madgraphMLM-pythia8/RunIISummer20UL16RECOAPV-106X_mcRun2_asymptotic_preVFP_v8-v2/AODSIM'
+			,'/TTJets_SingleLeptFromTbar_TuneCP5_13TeV-madgraphMLM-pythia8/RunIISummer20UL16RECOAPV-106X_mcRun2_asymptotic_preVFP_v8-v2/AODSIM'
+			]
+datasets = [
+			'/MET/Run2016B-21Feb2020_ver1_UL2016_HIPM-v1/AOD'
+			,'/MET/Run2016B-21Feb2020_ver2_UL2016_HIPM-v1/AOD'
+			,'/MET/Run2016C-21Feb2020_UL2016_HIPM-v1/AOD'
+			,'/MET/Run2016D-21Feb2020_UL2016_HIPM-v1/AOD'
+			,'/MET/Run2016E-21Feb2020_UL2016_HIPM-v1/AOD'
+			,'/MET/Run2016F-21Feb2020_UL2016_HIPM-v1/AOD'
+			,'/MET/Run2016F-21Feb2020_UL2016-v1/AOD'
+			,'/MET/Run2016G-21Feb2020_UL2016-v1/AOD'
+			,'/MET/Run2016H-21Feb2020_UL2016-v2/AOD'
 			]
 
-### for back. files on pnfs	
 def submitToCrab():
 	nJobs = 0
 	for ijob, dataset in enumerate(datasets):
 
-		tag = ((dataset.split('/')[1]).split("_")[0])+((dataset.split('/')[1]).split("_")[1])+(dataset.split('/')[2]).split("-")[0]
-		#tag = dataset.split('/')[1]+(dataset.split('/')[2]).split("-")[0]
+		#tag = ((dataset.split('/')[1]).split("_")[0])+((dataset.split('/')[1]).split("_")[1])+(dataset.split('/')[2]).split("-")[0]
+		tag = ((dataset.split('/')[1]).split("/")[0])+"_Run"+(dataset.split('/Run')[1]).split("/")[0]
 		print "use tag:", tag
 
 		if os.path.exists("crab_projects/crab_" + tag): 
 			shutil.rmtree("crab_projects/crab_" + tag)
-			#os.rmdir("crab_projects/crab_" + tag)
 		if os.path.exists("crabSubmissionScript_"+str(ijob)+".py"): os.remove("crabSubmissionScript_"+str(ijob)+".py")
 		
 		f = open("crabSubmissionScript_"+str(ijob)+".py", "w")
@@ -43,14 +66,14 @@ def submitToCrab():
 		f.write("import glob\n")
 		f.write("now = datetime.datetime.now()\n")
 		f.write("config = config()\n")
-		#f.write("config.General.requestName = \"" + tag +"_ \" +now.strftime(\"%Y_%m_%d\")\n")
 		f.write("config.General.requestName = \"" + tag +"\"\n")
 		f.write("config.General.workArea = \"crab_projects\"\n")
 		f.write("config.General.transferOutputs = True\n")
 		f.write("config.General.transferLogs = True\n")
 		f.write("config.JobType.pluginName = \"Analysis\"\n")
 		f.write("config.JobType.psetName = \"construct_secondary_vertices_cfg.py\"\n")
-		f.write("config.JobType.scriptArgs = [\"tag=crab,genmatchalltracks,era16_UL_APV\" , \"dataset="+ dataset + "\"]\n")
+		#f.write("config.JobType.scriptArgs = [\"tag=crab,genmatchalltracks,era16_UL_APV\" , \"dataset="+ dataset + "\"]\n")
+		f.write("config.JobType.scriptArgs = [\"tag=crab,data,era16_UL_APV\"]\n")
 		f.write("config.JobType.inputFiles = [\"/nfs/dust/cms/user/tewsalex/CMSSW_10_2_18/src/ntuplizer/plantTrees.py\",\n")
 		f.write("\"/nfs/dust/cms/user/tewsalex/CMSSW_10_2_18/src/ntuplizer/commons.py\",\n")
 		f.write("\"/nfs/dust/cms/user/wolfmor/NTupleStuff/\"]\n")
@@ -61,11 +84,12 @@ def submitToCrab():
 		f.write("config.Data.inputDBS = \"global\"\n")
 		f.write("config.Data.splitting = \"FileBased\"\n")
 		f.write("config.Data.unitsPerJob = 1\n")
+		f.write("config.Data.outLFNDirBase = \"/store/user/altews/NTuples/NTuplesVXXX/16UL_preAPV/\"\n")
 		if limitNJobs:
 			f.write("NJOBS = 1 \n")
 			f.write("config.Data.totalUnits = config.Data.unitsPerJob * NJOBS \n")
 		f.write("config.Data.publication = False \n")
-		f.write("config.Data.outputDatasetTag = \"" + tag +"_\" +now.strftime(\"%Y_%m_%d\")\n")
+		f.write("config.Data.outputDatasetTag = now.strftime(\"%Y_%m_%d\")\n")
 		f.write("config.Site.storageSite = \"T2_DE_DESY\"\n")
 		f.close()
 
